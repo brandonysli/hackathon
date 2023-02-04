8f54aeacfe0a2ebe0cd45259809bdfc262472b1e
mikeyliang <liangmichael27822@gmail.com>
2023-02-04 14:47:28 -0500

    fix

--- src/pages/components/Post.js
+++ src/pages/components/Post.js
@@ -8,12 +8,12 @@ export default function Post() {
 
     return ( 
 
-        <div className="flex flex-row gap-4 p-4 font-OpenSans">
+        <div className="flex flex-row w-3/4 gap-4 p-4 font-OpenSans">
             <NextImage src={img} width={200} height = {100} className="rounded-md"></NextImage>
             <div className="flex flex-col gap-2">
                 <div className="flex flex-row justify-between gap-8">
                     <div className="flex flex-col gap-2">
-                        <div className="flex flex-row items-center gap-2">
+                        <div className="flex flex-row items-center gap-2 font-medium">
                             <div className="flex px-2 py-1 bg-red-300 rounded-md">
                                 <p>Illness</p>
                             </div>
@@ -24,27 +24,27 @@ export default function Post() {
                                 <p>Illness</p>
                             </div>
                         </div>
-                        <h1>Dog Needs </h1>
+                        <h1 className="text-2xl font-semibold">Dog Needs </h1>
                     </div>
                     <IconBookmark className="mt-2 mr-2"/>
                 </div>
                 <div className="flex flex-col gap-4">
                     <div className="flex flex-row">
-                        <div className="flex px-2 py-1 bg-gray-100 border-2 border-gray-200 rounded-md">
-                            <p>Review by 2/6/2020</p>
+                        <div className="flex px-2 py-1 bg-gray-100 rounded-md">
+                            <p className="text-sm font-bold text-gray-500">Review by 2/6/2020</p>
                         </div>
                     </div>
                     <div className="flex flex-row items-center gap-2">
-                        <p>Dr. Brandon Li</p>
+                        <p className="font-semibold text-gray-600">Dr. Brandon Li</p>
                         <div className="flex px-2 py-1 bg-purple-300 rounded-md">
-                            <p>Orthodontist</p>
+                            <p className="text-xs font-semibold">Orthodontist</p>
                         </div>
                     </div>
 
                 </div>
 
-                <div className="flex flex-row justify-between">
-                    <p> uploaded 10 min ago</p>
+                <div className="flex flex-row justify-between mt-4">
+                    <p className="text-base font-medium"> uploaded 10 min ago</p>
                 </div>
             </div>
         </div>
