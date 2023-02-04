e2cd838e47b2840249adc7102f56f84410dafe0a
Brandon Li <byl23@cornell.edu>
2023-02-04 17:12:22 -0500

    trying to fix push XD

---
+++ src/pages/api/user.js
@@ -0,0 +1,38 @@
+
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  if (req.method == 'GET') {
+    try {
+      const data = JSON.stringify(req);
+      const posts = prisma.user.findMany({
+        where: {
+          userId: data.id
+        }
+      });
+      res.status(200).json(posts);
+    } catch {
+      res.status(500).json({
+        message: error
+      });
+    }
+  } else if (req.method == 'POST') {
+    try {
+      const data = JSON.stringify(req);
+      const post = await prisma.post.create({
+        data: {
+          description: data.description,
+          title: data.title,
+          user: data.user,
+          rating: data.rating
+        }
+      });
+      res.status(200).json(post);
+    } catch {
+      res.status(500).json({
+        message: error
+      });
+    }
+  }
+}
